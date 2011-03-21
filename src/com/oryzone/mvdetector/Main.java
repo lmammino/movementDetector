ec861946563f9c8dfb0cd22856c49b81908c5e4e
Luciano Mammino <lmammino@oryzone.com>
2011-03-21 02:52:27 +0100

    Creating user interface. Heavily unstable at the moment

--- src/com/oryzone/mvdetector/Main.java
+++ src/com/oryzone/mvdetector/Main.java
@@ -1,6 +1,7 @@
 package com.oryzone.mvdetector;
 
 import com.oryzone.mvdetector.gui.MainWindow;
+import javax.swing.UIManager;
 
 /**
  * Class that runs up the application
@@ -10,6 +11,7 @@ import com.oryzone.mvdetector.gui.MainWindow;
  */
 public class Main
 {
+
     /**
      * Method called automatically as starting point of the application
      * 
@@ -17,14 +19,66 @@ public class Main
      *            console args
      * @throws Exception
      */
-    ///TODO: Catch all the generic exceptions and provide a method to handle them (logging and reporting)
-    public static void main(String[] args) throws Exception
+    public static void main(String[] args)
     {
-	//Detector d = new Detector();
-	//d.start();
-	
-	MainWindow w = new MainWindow();
-	w.show();
-	w.getDisplay().dispose();
+        Main.makeAppNative();
+        Main.setSystemLookAndFeel();
+
+        java.awt.EventQueue.invokeLater(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                new MainWindow().setVisible(true);
+            }
+
+        });
     }
+    
+
+    /**
+     * Sets the system look and feel
+     */
+    protected static void setSystemLookAndFeel()
+    {
+        try
+        {
+            // set up default l&f and remember Menu Bar UI
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            String mbUI = UIManager.getString("MenuBarUI");
+
+            // set up our custom l&f, then override Menu Bar UI
+            UIManager.put("MenuBarUI", mbUI);
+        } catch (Exception e)
+        {
+            System.err.println("Cannot set system Look and Feel");
+        }
+    }
+
+
+    /**
+     * Executes code to make the application look more native on different
+     * operative systems
+     */
+    protected static void makeAppNative()
+    {
+        String lcOSName = System.getProperty("os.name").toLowerCase();
+        boolean IS_OSX = lcOSName.startsWith("mac os x");
+
+        if( IS_OSX )
+            Main.makeAppOSXNative();
+
+    }
+
+
+    /**
+     * Make the app OSX native
+     */
+    private static void makeAppOSXNative()
+    {
+        System.setProperty("apple.laf.useScreenMenuBar", "true");
+        System.setProperty("com.apple.macos.smallTabs", "true");
+        System.setProperty("com.apple.mrj.application.apple.menu.about.name", AppInfo.NAME);
+    }
+
 }
